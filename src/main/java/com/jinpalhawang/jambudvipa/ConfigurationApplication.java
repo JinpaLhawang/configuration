1d3e923a80da26f0f4147120dfe666fe92a8f789
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-05-01 19:53:07 -0400

    Logging.

--- src/main/java/com/jinpalhawang/jambudvipa/ConfigurationApplication.java
+++ src/main/java/com/jinpalhawang/jambudvipa/ConfigurationApplication.java
@@ -1,5 +1,7 @@
 package com.jinpalhawang.jambudvipa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,8 +12,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigurationApplication {
 
+  private static final Logger log = LoggerFactory.getLogger(ConfigurationApplication.class);
+
   public static void main(String[] args) {
-    SpringApplication.run(ConfigurationApplication.class, args);
+    final SpringApplication app = new SpringApplication(ConfigurationApplication.class);
+    app.setLogStartupInfo(false);
+    app.run(args);
+    log.info(ConfigurationApplication.class.getSimpleName() + " started...");
   }
 
 }
