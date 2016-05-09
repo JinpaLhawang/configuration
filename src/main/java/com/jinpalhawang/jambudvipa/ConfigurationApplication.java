82a70074ac7efc656d76cbbcb27dfd3e6c6dc3ed
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-05-09 13:29:45 -0400

    Revert logging.

--- src/main/java/com/jinpalhawang/jambudvipa/ConfigurationApplication.java
+++ src/main/java/com/jinpalhawang/jambudvipa/ConfigurationApplication.java
@@ -1,7 +1,5 @@
 package com.jinpalhawang.jambudvipa;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,13 +10,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigurationApplication {
 
-  private static final Logger log = LoggerFactory.getLogger(ConfigurationApplication.class);
-
   public static void main(String[] args) {
-    final SpringApplication app = new SpringApplication(ConfigurationApplication.class);
-    app.setLogStartupInfo(false);
-    app.run(args);
-    log.info(ConfigurationApplication.class.getSimpleName() + " started.");
+    SpringApplication.run(ConfigurationApplication.class, args);
   }
 
 }
